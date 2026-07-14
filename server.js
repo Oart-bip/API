19314495e6b8a4c3633cc8a1f3410799e4d8758d
Arthur <arthurnantestrento@gmail.com>
2026-07-13 21:14:42 -0300

    post

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express' // chamamos a biblioteca express 
 
-const app = express() // criamos a variavel app e transformamos a biblioteca express em uma funcao  
+const app = express() // criamos a variavel app e transformamos a biblioteca express em uma funcao   
+app.use(express.json()) // linha que permite com que o express leia arquivos Json
 
-app.get('/usuarios', (req, res) => {
-    res.send('Ok, deu bom') // metodo send do express - resposta
+const users = [] //array de memoria 
+
+app.post('/usuarios', (req, res) => { //aqui na rota post eu irei salvar meus usuarios
+
+    users.push(req.body)
+
+    res.send('Ok aqui deu certo') 
+
+})
+
+app.get('/usuarios', (req, res) => { // aqui na rota get eu vou listar meu usuarios 
+    res.json(users) // metodo send do express - resposta
 }) 
 // rota que esta listando a rota de usuarios com o metodo HTTP: GET -- req: request / res: response
 // => {} eh padrao do express 
