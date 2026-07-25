ed397ab0cadfaaac2b39cf8c948084f29cf4f768
Arthur <arthurnantestrento@gmail.com>
2026-07-24 21:19:37 -0300

    usuarios

--- FrontEnd/cadastro-clientes/src/pages/home/index.jsx
+++ FrontEnd/cadastro-clientes/src/pages/home/index.jsx
@@ -1,9 +1,25 @@
-
+import lixeira from '../../assets/lixeira.png'
 import './style.css'
 
 function Home() {
-// react a funcao sempre letra maiuscula 
-  
+  // react a funcao sempre letra maiuscula 
+
+  const users = [{ // array e seus objetos. JS  
+    id: '3131dasdasds',
+    name: 'Rodolfo',
+    age: 33,
+    email: 'rod@email.com'
+  },
+
+  {
+    id: '777575hghgh',
+    name: 'Aline',
+    age: 25,
+    email: 'aline@email.com'
+  },
+
+  ]
+
   return (
     <div className='container'>
       <forms>
@@ -11,8 +27,24 @@ function Home() {
         <input name='nome' type='text'></input>
         <input name='idade' type='number'></input>
         <input name='email' type='email'></input>
-        <button type='button'>Cadastrar</button> 
+        <button type='button'>Cadastrar</button>
       </forms>
+
+      {users.map(user => ( // to pegando os meus usuarios, mapeando com o map e guardando dentro do user
+        <div key={user.id}>
+          <div>
+            <p>Nome:{user.name} </p>
+            <p>Idade:{user.age} </p>
+            <p>Email:{user.email} </p>
+          </div>
+          <button>
+            <img src={lixeira} />
+          </button>
+        </div>
+      ))}
+
+
+
     </div>
   )
 }
